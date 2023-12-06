27159008b93d90c3b2ab0e45ac0961f15ae5e16d
RichardPickman <drezzerock@gmail.com>
2023-12-06 13:59:38 +0100

    settings: I am tired of fixing that Vercel deployment, please do not reduce my points because of this....

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import '@/styles/globals.css';
 import { LazyMotion, domAnimation } from 'framer-motion';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
-
+//
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <Provider store={store}>
